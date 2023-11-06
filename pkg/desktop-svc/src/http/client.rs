e94752423664cb28fa658bfccb6357b318809522
zzq <i@zhangzqs.cn>
2023-11-06 21:02:34 +0800

    add embedded http client impl on desktop

--- pkg/desktop-svc/src/http/client.rs
+++ pkg/desktop-svc/src/http/client.rs
@@ -1,64 +1,53 @@
-use embedded_io::ErrorType;
-use embedded_io::{Read, Write};
-use embedded_svc::http::client::Connection;
+use std::io::Read;
+use std::str::FromStr;
+
+use embedded_io::{ErrorType, Write as _};
+use embedded_svc::http::client::{Client, Connection};
 use embedded_svc::http::Method;
 use embedded_svc::http::Status;
 use reqwest;
+use reqwest::header::{HeaderMap, HeaderName};
 
-struct NewType<T>(pub T);
-
-impl From<embedded_svc::http::Method> for NewType<reqwest::Method> {
-    fn from(method: Method) -> Self {
-        let m = match method {
-            Method::Get => reqwest::Method::GET,
-            Method::Post => reqwest::Method::POST,
-            Method::Put => reqwest::Method::PUT,
-            Method::Delete => reqwest::Method::DELETE,
-            Method::Head => reqwest::Method::HEAD,
-            Method::Options => reqwest::Method::OPTIONS,
-            Method::Connect => reqwest::Method::CONNECT,
-            Method::Patch => reqwest::Method::PATCH,
-            Method::Trace => reqwest::Method::TRACE,
-            _ => panic!("Unsupported method: {:?}", method),
-        };
-        Self(m)
+fn method_type_convert(method: Method) -> reqwest::Method {
+    match method {
+        Method::Get => reqwest::Method::GET,
+        Method::Post => reqwest::Method::POST,
+        Method::Put => reqwest::Method::PUT,
+        Method::Delete => reqwest::Method::DELETE,
+        Method::Head => reqwest::Method::HEAD,
+        Method::Options => reqwest::Method::OPTIONS,
+        Method::Connect => reqwest::Method::CONNECT,
+        Method::Patch => reqwest::Method::PATCH,
+        Method::Trace => reqwest::Method::TRACE,
+        _ => panic!("Unsupported method: {:?}", method),
     }
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-enum State {
-    New,
-    Request,
-    Response,
-}
-
 struct HttpClientAdapterConnection {
-    client: reqwest::Client,
-    state: State,
+    client: reqwest::blocking::Client,
+    request: Option<reqwest::blocking::RequestBuilder>,
+    response: Option<reqwest::blocking::Response>,
+    req_buffer: Vec<u8>,
 }
 
 impl HttpClientAdapterConnection {
     fn new() -> Self {
         Self {
-            client: reqwest::Client::new(),
-            state: State::New,
-        }
-    }
-
-    fn assert_initial(&self) {
-        if self.state != State::New && self.state != State::Response {
-            panic!("connection is not in initial phase");
+            client: reqwest::blocking::Client::new(),
+            request: None,
+            response: None,
+            req_buffer: Vec::new(),
         }
     }
 
     fn assert_request(&self) {
-        if self.state != State::Request {
+        if self.request.is_none() {
             panic!("connection is not in request phase");
         }
     }
 
     fn assert_response(&self) {
-        if self.state != State::Response {
+        if self.response.is_none() {
             panic!("connection is not in response phase");
         }
     }
@@ -66,28 +55,49 @@ impl HttpClientAdapterConnection {
 
 impl embedded_svc::http::Headers for HttpClientAdapterConnection {
     fn header(&self, name: &str) -> Option<&'_ str> {
-        todo!()
+        self.assert_response();
+        self.response
+            .as_ref()
+            .unwrap()
+            .headers()
+            .get(name)
+            .map(|v| v.to_str().unwrap())
     }
 }
 
 impl Status for HttpClientAdapterConnection {
     fn status(&self) -> u16 {
-        todo!()
+        self.assert_response();
+        self.response.as_ref().unwrap().status().as_u16()
     }
 
     fn status_message(&self) -> Option<&'_ str> {
-        todo!()
+        self.assert_response();
+        self.response.as_ref().unwrap().status().canonical_reason()
     }
 }
 
 #[derive(Debug)]
-struct HttpClientAdapterConnectionError {
-    error: reqwest::Error,
+enum HttpClientAdapterConnectionError {
+    ReqwestError(reqwest::Error),
+    Other(Box<dyn std::error::Error>),
+}
+
+impl From<Box<dyn std::error::Error>> for HttpClientAdapterConnectionError {
+    fn from(error: Box<dyn std::error::Error>) -> Self {
+        Self::Other(error)
+    }
+}
+
+impl From<reqwest::Error> for HttpClientAdapterConnectionError {
+    fn from(error: reqwest::Error) -> Self {
+        Self::ReqwestError(error)
+    }
 }
 
 impl embedded_io::Error for HttpClientAdapterConnectionError {
     fn kind(&self) -> embedded_io::ErrorKind {
-        todo!()
+        embedded_io::ErrorKind::Other
     }
 }
 
@@ -95,19 +105,25 @@ impl ErrorType for HttpClientAdapterConnection {
     type Error = HttpClientAdapterConnectionError;
 }
 
-impl Read for HttpClientAdapterConnection {
+impl embedded_io::Read for HttpClientAdapterConnection {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
-        todo!()
+        self.assert_response();
+        let u = self.response.as_mut().unwrap().read(buf).unwrap();
+        Ok(u)
     }
 }
 
-impl Write for HttpClientAdapterConnection {
+impl embedded_io::Write for HttpClientAdapterConnection {
     fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
-        todo!()
+        self.assert_request();
+        self.req_buffer.extend_from_slice(buf);
+        Ok(buf.len())
     }
 
     fn flush(&mut self) -> Result<(), Self::Error> {
-        todo!()
+        self.assert_request();
+        self.request = Some(self.request.take().unwrap().body(self.req_buffer.clone()));
+        Ok(())
     }
 }
 
@@ -126,26 +142,72 @@ impl Connection for HttpClientAdapterConnection {
         uri: &'a str,
         headers: &'a [(&'a str, &'a str)],
     ) -> Result<(), Self::Error> {
-        todo!()
+        let mut hs = HeaderMap::new();
+        for (k, v) in headers {
+            let k = HeaderName::from_str(k).unwrap();
+            let v = v.parse().unwrap();
+            hs.insert(k, v);
+        }
+        self.request = Some(
+            self.client
+                .request(method_type_convert(method), uri)
+                .headers(hs),
+        );
+        Ok(())
     }
 
     fn is_request_initiated(&self) -> bool {
-        todo!()
+        self.request.is_some()
     }
 
     fn initiate_response(&mut self) -> Result<(), Self::Error> {
-        todo!()
+        self.assert_request();
+        self.flush()?;
+
+        let req_builder = self.request.take().unwrap();
+        let req = req_builder.build()?;
+        let resp = self.client.execute(req)?;
+        self.response = Some(resp);
+        Ok(())
     }
 
     fn is_response_initiated(&self) -> bool {
-        todo!()
+        self.response.is_some()
     }
 
     fn split(&mut self) -> (&Self::Headers, &mut Self::Read) {
-        todo!()
+        todo!("unimplemented")
     }
 
     fn raw_connection(&mut self) -> Result<&mut Self::RawConnection, Self::Error> {
-        todo!()
+        Ok(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use embedded_svc::utils::io;
+
+    use super::*;
+
+    #[test]
+    fn test_http_client_adapter() {
+        let conn = HttpClientAdapterConnection::new();
+        // Prepare headers and URL
+        let headers = [("accept", "text/plain")];
+        let url: &str = "http://ifconfig.net/";
+        let mut client = Client::<HttpClientAdapterConnection>::wrap(conn);
+        let req = client.request(Method::Get, url, &headers).unwrap();
+        println!("-> GET {}", url);
+        let mut resp = req.submit().unwrap();
+        println!("<- {} {}", resp.status(), resp.status_message().unwrap());
+        assert_eq!(resp.status(), 200);
+        let mut buf = [0u8; 1024];
+        let br = io::try_read_full(&mut resp, &mut buf).unwrap();
+        println!("Read {} bytes", br);
+        let s = std::str::from_utf8(&buf[..br]).unwrap();
+        println!("Response body (truncated to {} bytes): {:?}", buf.len(), s);
+        // Drain the remaining response bytes
+        while resp.read(&mut buf).unwrap() > 0 {}
     }
 }
--- pkg/http-client/http-client-embedded/src/lib.rs
+++ pkg/http-client/http-client-embedded/src/lib.rs
@@ -1,14 +1,61 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use anyhow::Result;
+use embedded_svc::{http::client, utils::io};
+use http::{Request, Response};
+
+pub struct HttpClient<C: client::Connection> {
+    client: client::Client<C>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<C: client::Connection> HttpClient<C> {
+    pub fn new(client: client::Client<C>) -> Self {
+        Self { client }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<C: client::Connection> http_client_common::Client for HttpClient<C> {
+    fn send(&mut self, req: Request<&[u8]>) -> Result<Response<Vec<u8>>> {
+        let uri = req.uri().to_string();
+        let headers = req
+            .headers()
+            .iter()
+            .map(|(k, v)| (k, String::from_utf8(v.as_bytes().to_vec()).unwrap()))
+            .collect::<Vec<_>>();
+        let headers = headers
+            .iter()
+            .map(|(k, v)| (k.as_str(), v.as_str()))
+            .collect::<Vec<_>>();
+        let h = headers.as_slice();
+        let method = match *req.method() {
+            http::Method::GET => client::Method::Get,
+            http::Method::POST => client::Method::Post,
+            http::Method::PUT => client::Method::Put,
+            http::Method::DELETE => client::Method::Delete,
+            http::Method::HEAD => client::Method::Head,
+            http::Method::OPTIONS => client::Method::Options,
+            http::Method::CONNECT => client::Method::Connect,
+            http::Method::PATCH => client::Method::Patch,
+            http::Method::TRACE => client::Method::Trace,
+            _ => unreachable!(),
+        };
+        let mut request = self.client.request(method, &uri, h).unwrap();
+        request.write(req.body()).unwrap();
+        let mut response = request.submit().unwrap();
+        let (a, b) = response.split();
+
+        let a = Response::builder()
+            .status(response.status())
+            .body({
+                let mut ret = Vec::<u8>::new();
+                let mut buf = [0u8; 512];
+                while let Ok(l) = response.read(&mut buf) {
+                    if l == 0 {
+                        break;
+                    }
+                    ret.extend_from_slice(&buf[..l]);
+                }
+                ret
+            })
+            .unwrap();
+        Ok(a)
     }
 }

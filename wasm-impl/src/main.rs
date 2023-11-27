5aa7e9bc52a2c88cfd4e3568ba6481071871bbfa
zzq <i@zhangzqs.cn>
2023-11-27 09:23:21 +0800

    remove wasm

--- wasm-impl/src/main.rs
+++ wasm-impl/src/main.rs
@@ -24,35 +24,4 @@ pub fn main() {
     let mut physical_display = Arc::new(Mutex::new(Framebuffer::<Rgb888, _, BigEndian, 240, 240, 172800>::new()));
 
     let display_group = Arc::new(Mutex::new(DisplayGroup::new(physical_display.clone(), 2)));
-
-    let fps = Rc::new(RefCell::new(0));
-    {
-        let fps_ref = fps.clone();
-        let slint_display = LogicalDisplay::new(
-            display_group.clone(),
-            Rectangle {
-                top_left: Point::new(0, 0),
-                size: Size::new(240, 240),
-            },
-        );
-        let slint_display_id = slint_display.lock().unwrap().get_id() as isize;
-        display_group
-            .lock()
-            .unwrap()
-            .switch_to_logical_display(slint_display_id);
-
-        let platform = EmbeddedSoftwarePlatform::<_, _, _, _, RGB888PixelColorAdapter>::new(
-            slint_display,
-            Some(Box::new(move |has_redraw| {
-                if has_redraw {
-                    *fps_ref.borrow_mut() += 1;
-                }
-                Ok(())
-            })),
-        );
-        slint::platform::set_platform(Box::new(platform)).unwrap();
-    }
-    info!("platform has been set");
-
-    
 }

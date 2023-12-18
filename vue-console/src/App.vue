08634154688cfe0b0b63d4bdb1166d478f3b3350
zzq <i@zhangzqs.cn>
2023-12-18 21:29:41 +0800

    add

--- vue-console/src/App.vue
+++ vue-console/src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { Axios } from 'axios'
+const login = () => {
+    const axios = new Axios()
+    axios.get('/api/login')
+
+    console.log('login')
+}
 </script>
 
 <template>
+    <button @click="login">登录</button>
     <router-view></router-view>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>

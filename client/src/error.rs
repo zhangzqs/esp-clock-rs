65b68a799d926328097d5d99d5dec53824856a72
zzq <i@zhangzqs.cn>
2024-01-06 03:00:02 +0800

    refactor

---
+++ client/src/error.rs
@@ -0,0 +1,24 @@
+use std::fmt::Display;
+
+use embedded_svc::http::client::Connection;
+
+#[derive(thiserror::Error, Debug)]
+pub enum ClientError<C, E>
+where
+    C: Connection<Error = E>,
+    E: Display,
+{
+    #[error("http error: {0}")]
+    Http(C::Error),
+    #[error("json error: {0}")]
+    Json(#[from] serde_json::Error),
+    #[error("io error: {0}")]
+    ReadExactError(#[from] embedded_io::ReadExactError<E>),
+    #[error("unknown content length, header: {0:?}")]
+    UnknownContentLength(Option<String>),
+    #[error("buffer too small, content length: {content_length}, buffer length: {buffer_length}")]
+    BufferTooSmall {
+        content_length: usize,
+        buffer_length: usize,
+    },
+}
--- client/src/weather/client.rs
+++ client/src/weather/client.rs
@@ -5,6 +5,8 @@ use embedded_svc::{
     io::Read,
 };
 
+use crate::ClientError;
+
 use super::{WeatherCityLookupResponse, WeatherNowResponse};
 
 pub struct WeatherClient<'a, C> {
@@ -18,46 +20,25 @@ impl<'a, C> WeatherClient<'a, C> {
     }
 }
 
-#[derive(thiserror::Error, Debug)]
-pub enum WeatherError<C, E>
-where
-    C: Connection<Error = E>,
-    E: Display,
-{
-    #[error("http error: {0}")]
-    Http(C::Error),
-    #[error("json error: {0}")]
-    Json(#[from] serde_json::Error),
-    #[error("io error: {0}")]
-    ReadExactError(#[from] embedded_io::ReadExactError<E>),
-    #[error("unknown content length, header: {0:?}")]
-    UnknownContentLength(Option<String>),
-    #[error("buffer too small, content length: {content_length}, buffer length: {buffer_length}")]
-    BufferTooSmall {
-        content_length: usize,
-        buffer_length: usize,
-    },
-}
-
 impl<C, E> WeatherClient<'_, C>
 where
     C: Connection<Error = E>,
     E: Debug + Display,
 {
-    fn get<T, const S: usize>(&mut self, url: &str) -> Result<T, WeatherError<C, E>>
+    fn get<T, const S: usize>(&mut self, url: &str) -> Result<T, ClientError<C, E>>
     where
         T: serde::de::DeserializeOwned,
     {
-        let req = self.client.get(url).map_err(WeatherError::Http)?;
-        let mut resp = req.submit().map_err(WeatherError::Http)?;
+        let req = self.client.get(url).map_err(ClientError::Http)?;
+        let mut resp = req.submit().map_err(ClientError::Http)?;
         let coontent_length = resp
             .header("Content-Length")
-            .ok_or(WeatherError::UnknownContentLength(None))?;
+            .ok_or(ClientError::UnknownContentLength(None))?;
         let content_length = coontent_length
             .parse::<usize>()
-            .map_err(|_| WeatherError::UnknownContentLength(Some(coontent_length.to_string())))?;
+            .map_err(|_| ClientError::UnknownContentLength(Some(coontent_length.to_string())))?;
         if content_length > S {
-            return Err(WeatherError::BufferTooSmall {
+            return Err(ClientError::BufferTooSmall {
                 content_length,
                 buffer_length: S,
             });
@@ -71,7 +52,7 @@ where
     pub fn city_lookup<const S: usize>(
         &mut self,
         query: &str,
-    ) -> Result<WeatherCityLookupResponse, WeatherError<C, E>> {
+    ) -> Result<WeatherCityLookupResponse, ClientError<C, E>> {
         let url = format!("{}/api/weather/city_lookup?query={}", self.base_url, query);
         self.get::<_, S>(url.as_str())
     }
@@ -79,7 +60,7 @@ where
     pub fn now<const S: usize>(
         &mut self,
         city_id: &str,
-    ) -> Result<WeatherNowResponse, WeatherError<C, E>> {
+    ) -> Result<WeatherNowResponse, ClientError<C, E>> {
         let url = format!("{}/api/weather/now?city_id={}", self.base_url, city_id);
         self.get::<_, S>(url.as_str())
     }
